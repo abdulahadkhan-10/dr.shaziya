526a4bb680009d359cdcfb9b42c2473055d896c4
abdulahadkhan-10 <st.abdul24@gmail.com>
2025-11-04 13:17:39 +0530

    update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -59,7 +59,7 @@ export default function Home() {
                 className="text-blue-500 font-semibold text-xl mb-4 tracking-widest md:text-left"
                 // style={{ fontFamily: "Times New Roman, serif" }}
               >
-                DIPLOMA in HERNIA SURGERY Essentials <br />
+                DIPLOMA in HERNIA SURGERY  <br />
                 (By Asia Pacific Hernia Society & The Royal College of Surgeons
                 of Edinburgh)
               </p>
